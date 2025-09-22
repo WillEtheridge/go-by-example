32b21d1b8df50d0720d5483a675008f9a4212299
WillEtheridge <wjeth@proton.me>
2025-09-22 10:09:10 -0600

    x

--- maps/maps.go
+++ maps/maps.go
@@ -66,6 +66,6 @@ func main() {
 	// for maps
 	n2 := map[string]int{"foo": 1, "bar": 2}
 	if maps.Equal(n, n2) {
-		fmt.Println("n === n2") // n === n2
+		fmt.Println("n === n2") //  n === n2
 	}
 }
