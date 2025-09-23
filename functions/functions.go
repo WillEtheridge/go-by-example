5d9edc548ac0017566958259953dc9bfcba631eb
WillEtheridge <wjeth@proton.me>
2025-09-23 10:22:00 -0600

    23/09/25

---
+++ functions/functions.go
@@ -0,0 +1,35 @@
+/*
+* 10. Functions
+* Functions are central in Go
+* We'll learn about functions with a few different examples
+ */
+
+package main
+
+import "fmt"
+
+// Here's a function that takes two ints and returns their sum as an int
+func plus(a int, b int) int {
+	// Go requires explicit returns, i.e it won't automatically
+	// return the value of the last expression
+	return a + b
+}
+
+/*
+* When you have multiple consecutive paramaters of the same type
+* you may omit the type name for the like-typed parameters
+* up to the final param that declares the type
+ */
+
+func plusPlus(a, b, c int) int {
+	return a + b + c
+}
+
+// Call a function just as you would expect with name args
+func main() {
+	res := plus(1, 2)
+	fmt.Println("1+2 =", res)
+
+	res = plusPlus(1, 2, 3)
+	fmt.Println("1+2+3 =", res)
+}
